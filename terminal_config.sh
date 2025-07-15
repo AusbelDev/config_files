291878f3b1adea702cf4bb3bd98e8cc18c425e8f
Marcos "Ausbel" Rocha <ausbel.dev@gmail.com>
2025-07-15 13:46:25 -0600

    Update terminal_config.sh

--- terminal_config.sh
+++ terminal_config.sh
@@ -1,25 +1,85 @@
-sudo apt update
-sudo apt upgrade -y
+#!/bin/bash
 
-sudo apt install zsh
+set -e  # Exit on error
+set -u  # Treat unset variables as error
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-echo 'source <(fzf --zsh)' >> .zshrc
-cd
+# Colors
+GREEN="\e[32m"
+RED="\e[31m"
+NC="\e[0m"
 
-sudo apt install -y lsd
+log() {
+    echo -e "${GREEN}[+] $1${NC}"
+}
 
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+error() {
+    echo -e "${RED}[!] $1${NC}" >&2
+    exit 1
+}
 
-cd
+# Ensure script is run as normal user, not root
+if [ "$(id -u)" -eq 0 ]; then
+    error "Do not run this script as root. It will use sudo as needed."
+fi
 
-echo 'plug "romkatv/powerlevel10k"' >> .zshrc
+check_cmd() {
+    command -v "$1" >/dev/null 2>&1
+}
 
-echo 'plug "wintermi/zsh-lsd"' >> .zshrc
+add_zshrc_once() {
+    grep -qxF "$1" "$HOME/.zshrc" || echo "$1" >> "$HOME/.zshrc"
+}
 
-echo 'plug "zsh-users/zsh-syntax-highlighting"' >> .zshrc
+install_package() {
+    if ! dpkg -s "$1" >/dev/null 2>&1; then
+        log "Installing $1..."
+        sudo apt install -y "$1"
+    else
+        log "$1 already installed"
+    fi
+}
 
-echo 'plug "zsh-users/zsh-history-substring-search"' >> .zshrc
+log "Updating system..."
+sudo apt update && sudo apt upgrade -y
 
-echo 'plug "Aloxaf/fzf-tab"' >> .zshrc
+# Install required packages
+for pkg in sudo wget curl zsh git lsd; do
+    install_package "$pkg"
+done
+
+# Set zsh as default shell
+if [ "$SHELL" != "/usr/bin/zsh" ]; then
+    log "Setting Zsh as default shell..."
+    chsh -s /usr/bin/zsh
+fi
+
+# Install FZF
+if [ ! -d "$HOME/.fzf" ]; then
+    log "Installing fzf..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --key-bindings --completion --no-update-rc
+fi
+add_zshrc_once 'source ~/.fzf.zsh'
+
+# History settings
+add_zshrc_once 'HISTFILE=~/.zsh_history'
+add_zshrc_once 'HISTSIZE=10000'
+add_zshrc_once 'SAVEHIST=10000'
+add_zshrc_once 'setopt appendhistory'
+
+# Zap Zsh plugin manager
+if [ ! -d "$HOME/.local/share/zap" ]; then
+    log "Installing Zap..."
+    zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
+fi
+add_zshrc_once '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"'
+
+# Zap plugins
+add_zshrc_once 'plug "romkatv/powerlevel10k"'
+add_zshrc_once 'plug "wintermi/zsh-lsd"'
+add_zshrc_once 'plug "zsh-users/zsh-syntax-highlighting"'
+add_zshrc_once 'plug "zsh-users/zsh-history-substring-search"'
+add_zshrc_once 'plug "Aloxaf/fzf-tab"'
+
+log "Setup complete! Starting Zsh..."
+exec zsh
